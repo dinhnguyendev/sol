063b1d787a6230744eee587cf44a244b23f6f482
Nguyễn Ngọc Đỉnh <106016948+dinhnguyendev@users.noreply.github.com>
2022-11-25 17:59:02 +0700

    Update 2.sol

--- 2.sol
+++ 2.sol
@@ -1,34 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-contract Free{
+contract GameCaro{
     IERC20 public TOKENXU;
-    uint256 public amount=1000*10**18;
-    bool public isChecked;
-    address [] public usersClaimed;
-   
+    address private _owner; 
     constructor (address _tokenXu){
         TOKENXU=IERC20(_tokenXu);
+        _owner=msg.sender;
     }
-    function claim_TokenXu()public  {
-        require(checkUserClaim()==false,"sorry...you were reciver token xu ");
-        require(TOKENXU.balanceOf(address(this)) >= amount ,"sorry... amount...");
-        isChecked=TOKENXU.transfer(msg.sender,amount);
-        usersClaimed.push(msg.sender);
+    function wirthraw(uint256 amount)public payable  {
+        require(checkTotal() >= amount ,"sorry... amount...");
+        require( _owner == msg.sender,"sorry... admin...");
+        TOKENXU.transfer(msg.sender,amount);
+    }
+    function transferWinner(address winner,uint256 amount)public payable  {
+        require(checkTotal() >= amount ,"sorry... amount...");
+        require( _owner == msg.sender,"sorry... admin...");
+        TOKENXU.transfer(winner,amount);
     }
     function checkTotal() public view returns(uint256){
         uint256  total =TOKENXU.balanceOf(address(this));
         return  total;
     }
-    function checkUserClaim()public view returns(bool){
-      bool check=false;
-      for(uint count=0;count<usersClaimed.length;count ++){
-          if(usersClaimed[count]==msg.sender){
-              check=true;
-              break;
-          } 
-      }
-      return check;
-
+    function checkOwner() public view returns(address){
+        return  _owner;
     }
+   
 }
